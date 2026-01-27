f493e77acb0b5ac60b8e6a65eea400ec63f79f33
Thierry <Wolken049@gmail.com>
2026-01-27 08:21:50 +0200

    All changes

--- Functions/Functions/Sorting Algorithms/Example.py
+++ Functions/Functions/Sorting Algorithms/Example.py
@@ -1,7 +1,5 @@
 from Insertion_Sort import InsertSort
-from Quick_sort import Quick
 
 Numb = [5, 4, 7, 8, 6, 0, 9 ,1 , 2, 6, 2, 5, 6, 1]
 
 print(InsertSort(Numb))
-print(Quick(Numb))
--- Functions/Sorting Algorithms/Quick_sort.py
+++ Functions/Sorting Algorithms/Quick_sort.py
@@ -6,4 +6,16 @@ def partition(a, low, high):
         if a[j] <= pivot:
             i += 1
             a[i], a[j] = a[j], a[i]
-    a[i + 1], a[high]
+    a[i + 1], a[high] = a[high], a[i + 1]
+    
+    return i + 1
+
+def Quick(a, low = 0, high = None):
+    if high is None:
+        high = len(a) - 1
+    
+    if low < high:
+        pivot_index = partition(a, low, high)
+        Quick(a, low, pivot_index - 1)
+        Quick(a, pivot_index + 1, high)
+    return a
